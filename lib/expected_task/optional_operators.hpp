ddaccc2842a15065f412c8f2e3746bdb455ec3fd
julien <julien.lopez51@gmail.com>
2022-02-13 11:43:29 +0100

    Update optional_operators.hpp
    adding concept checks for optional operators

--- lib/expected_task/optional_operators.hpp
+++ lib/expected_task/optional_operators.hpp
@@ -2,32 +2,44 @@
 
 #include <boost/optional.hpp>
 
-template <class T, class F> auto operator>=(boost::optional<T>&& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>=(boost::optional<T>&& opt, F f)
 {
     return opt.map(std::move(f));
 }
 
-template <class T, class F> auto operator>=(boost::optional<T>& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>=(boost::optional<T>& opt, F f)
 {
     return opt.map(std::move(f));
 }
 
-template <class T, class F> auto operator>=(const boost::optional<T>& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>=(const boost::optional<T>& opt, F f)
 {
     return opt.map(std::move(f));
 }
 
-template <class T, class F> auto operator>>=(boost::optional<T>&& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>>=(boost::optional<T>&& opt, F f)
 {
     return opt.flat_map(std::move(f));
 }
 
-template <class T, class F> auto operator>>=(boost::optional<T>& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>>=(boost::optional<T>& opt, F f)
 {
     return opt.flat_map(std::move(f));
 }
 
-template <class T, class F> auto operator>>=(const boost::optional<T>& opt, F f)
+template <class T, class F>
+requires std::invocable<F, T>
+auto operator>>=(const boost::optional<T>& opt, F f)
 {
     return opt.flat_map(std::move(f));
 }
